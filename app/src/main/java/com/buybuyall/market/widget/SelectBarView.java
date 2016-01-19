74da408f02c8a78fe978a25108520eafbf54d5c7
jake <903475400@qq.com>
2016-01-19 18:22:46 +0800

    优化筛选条

--- app/src/main/java/com/buybuyall/market/widget/SelectBarView.java
+++ app/src/main/java/com/buybuyall/market/widget/SelectBarView.java
@@ -21,6 +21,12 @@ import cn.common.utils.ViewUtil;
  * 描述：筛选的控件 作者：jake on 2016/1/17 21:11
  */
 public class SelectBarView extends LinearLayout implements View.OnClickListener {
+    public static final int SORT_DEFAULT = -1;
+
+    public static final int SORT_RUSH_BUY = 1;
+
+    public static final int SORT_DISCOUNT = 2;
+
     private TextView tvNew;
 
     private View vSort;
@@ -31,6 +37,8 @@ public class SelectBarView extends LinearLayout implements View.OnClickListener 
 
     private TextView tvPrize;
 
+    private TextView tvHasSortName;
+
     private ImageView ivUp;
 
     private ImageView ivDown;
@@ -39,18 +47,28 @@ public class SelectBarView extends LinearLayout implements View.OnClickListener 
 
     private int lastId;
 
-    private boolean lastIsUp = false;
+    private int sortType;
+
+    private View vHasSort;
+
+    private PopupWindowHelper helper;
+
+    private CheckBox cbRushBuy;
+
+    private CheckBox cbDiscount;
+
+    private TextView tvSortName;
+
+    private View vTop;
 
     public interface IListener {
-        void selectNew();
+        void selectNew(int sortType);
 
-        void selectPrizeUp();
+        void selectPrizeUp(int sortType);
 
-        void selectPrizeDown();
+        void selectPrizeDown(int sortType);
 
-        void selectSales();
-
-        void sort(boolean isRushBuy);
+        void selectSales(int sortType);
     }
 
     public void setListener(IListener listener) {
@@ -63,74 +81,77 @@ public class SelectBarView extends LinearLayout implements View.OnClickListener 
 
     public SelectBarView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setOrientation(HORIZONTAL);
+        setOrientation(VERTICAL);
         setGravity(Gravity.CENTER_VERTICAL);
         inflate(context, R.layout.view_select_bar, this);
         tvNew = (TextView) findViewById(R.id.tv_new);
         tvSales = (TextView) findViewById(R.id.tv_sales);
-        vSort = findViewById(R.id.fl_sort);
-        vPrize = findViewById(R.id.fl_prize);
+        tvPrize = (TextView) findViewById(R.id.tv_prize);
+        tvHasSortName = (TextView) findViewById(R.id.tv_sort_name);
         ivUp = (ImageView) findViewById(R.id.iv_up);
         ivDown = (ImageView) findViewById(R.id.iv_down);
+        vSort = findViewById(R.id.fl_sort);
         vPrize = findViewById(R.id.fl_prize);
-        tvPrize = (TextView) findViewById(R.id.tv_prize);
+        vHasSort = findViewById(R.id.rl_sort);
         vPrize.setOnClickListener(this);
         tvNew.setOnClickListener(this);
         tvSales.setOnClickListener(this);
         vSort.setOnClickListener(this);
-        showSelectState(0);
-
+        findViewById(R.id.btn_clear).setOnClickListener(this);
+        changeState(0);
+        sortType = SORT_DEFAULT;
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
         if (id == R.id.tv_new) {
-            showSelectState(0);
+            changeState(0);
+            click(0);
             lastId = 0;
-            if (listener != null) {
-                listener.selectNew();
-            }
         } else if (id == R.id.tv_sales) {
-            showSelectState(2);
+            changeState(2);
             lastId = 2;
-            if (listener != null) {
-                listener.selectSales();
-            }
+            click(2);
         } else if (id == R.id.fl_prize) {
-            showSelectState(1);
+            changeState(1);
             lastId = 1;
-            if (lastIsUp) {
-                if (listener != null) {
-                    listener.selectPrizeUp();
-                }
-                ivUp.setSelected(true);
-                ivDown.setSelected(false);
-                lastIsUp = false;
-            } else {
-                if (listener != null) {
-                    listener.selectPrizeDown();
-                }
+            if (ivUp.isSelected()) {
                 ivUp.setSelected(false);
                 ivDown.setSelected(true);
-                lastIsUp = true;
+            } else {
+                ivUp.setSelected(true);
+                ivDown.setSelected(false);
             }
+            click(0);
         } else if (id == R.id.fl_sort) {
-            showSelectState(3);
+            changeState(3);
             if (helper == null) {
                 initPoPupWindow();
             }
             helper.showAsDropDown(vSort);
+        } else if (id == R.id.btn_ok) {
+            hidePop();
+            click(lastId);
+            if (sortType != SORT_DEFAULT) {
+                ViewUtil.setViewVisibility(vHasSort, VISIBLE);
+            }
+        } else if (id == R.id.v_shadow) {
+            hidePop();
+        } else if (id == R.id.btn_clear) {
+            sortType = SORT_DEFAULT;
+            ViewUtil.setViewVisibility(vHasSort, GONE);
+            cbRushBuy.setChecked(false);
+            cbDiscount.setChecked(false);
+            click(lastId);
         }
     }
 
-    private CheckBox cbRushBuy;
-
-    private CheckBox cbDiscount;
-
-    private TextView tvSortName;
-
-    private View vTop;
+    private void hidePop() {
+        if (helper != null) {
+            helper.dismiss();
+        }
+    }
 
     /**
      * 初始化popup window
@@ -139,42 +160,21 @@ public class SelectBarView extends LinearLayout implements View.OnClickListener 
         helper = new PopupWindowHelper(getContext());
         helper.setView(R.layout.pop_sort);
         helper.setMeasuredDimension(getWidth(), -2);
-        helper.setOnDismissListener(new PopupWindow.OnDismissListener() {
-            @Override
-            public void onDismiss() {
-                if (lastId == 1) {
-                    onClick(vPrize);
-                } else {
-                    showSelectState(lastId);
-                }
-            }
-        });
         tvSortName = (TextView) helper.findViewById(R.id.tv_sort_name);
         cbRushBuy = (CheckBox) helper.findViewById(R.id.cb_rush_buy);
         cbDiscount = (CheckBox) helper.findViewById(R.id.cb_discount);
         vTop = helper.findViewById(R.id.ll_top);
-        helper.findViewById(R.id.v_shadow).setOnClickListener(new OnClickListener() {
+        helper.findViewById(R.id.v_shadow).setOnClickListener(this);
+        helper.findViewById(R.id.btn_ok).setOnClickListener(this);
+        helper.setOnDismissListener(new PopupWindow.OnDismissListener() {
             @Override
-            public void onClick(View v) {
-                helper.dismiss();
-            }
-        });
-        helper.findViewById(R.id.btn_ok).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (cbRushBuy.isChecked()) {
-                    if (listener != null) {
-                        listener.sort(true);
-                    }
-                } else {
-                    if (listener != null) {
-                        listener.sort(false);
-                    }
+            public void onDismiss() {
+                if (lastId == 1) {
+                    ivUp.setSelected(true);
                 }
-                helper.dismiss();
+                changeState(lastId);
             }
         });
-        cbDiscount.setChecked(true);
         cbDiscount.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -182,6 +182,8 @@ public class SelectBarView extends LinearLayout implements View.OnClickListener 
                     ViewUtil.setViewVisibility(vTop, VISIBLE);
                     cbRushBuy.setChecked(false);
                     tvSortName.setText("限时折扣");
+                    tvHasSortName.setText("限时折扣");
+                    sortType = SORT_DISCOUNT;
                 } else {
                     if (!cbRushBuy.isChecked()) {
                         ViewUtil.setViewVisibility(vTop, GONE);
@@ -196,6 +198,8 @@ public class SelectBarView extends LinearLayout implements View.OnClickListener 
                     ViewUtil.setViewVisibility(vTop, VISIBLE);
                     cbDiscount.setChecked(false);
                     tvSortName.setText("抢购");
+                    tvHasSortName.setText("抢购");
+                    sortType = SORT_RUSH_BUY;
                 } else {
                     if (!cbDiscount.isChecked()) {
                         ViewUtil.setViewVisibility(vTop, GONE);
@@ -205,34 +209,53 @@ public class SelectBarView extends LinearLayout implements View.OnClickListener 
         });
     }
 
-    /**
-     * 显示选中状态
-     * 
-     * @param id
-     */
-    private void showSelectState(int id) {
+    private void changeState(int id) {
         tvNew.setSelected(false);
         tvSales.setSelected(false);
         tvPrize.setSelected(false);
         vSort.setSelected(false);
-        ivDown.setSelected(false);
-        ivUp.setSelected(false);
         switch (id) {
             case 0:
+                ivDown.setSelected(false);
+                ivUp.setSelected(false);
                 tvNew.setSelected(true);
                 break;
             case 1:
                 tvPrize.setSelected(true);
                 break;
             case 2:
+                ivDown.setSelected(false);
+                ivUp.setSelected(false);
                 tvSales.setSelected(true);
                 break;
             case 3:
+                ivDown.setSelected(false);
+                ivUp.setSelected(false);
                 vSort.setSelected(true);
                 break;
         }
 
     }
 
-    private PopupWindowHelper helper;
+    private void click(int id) {
+        if (listener == null) {
+            return;
+        }
+        switch (id) {
+            case 0:
+                listener.selectNew(sortType);
+                break;
+            case 1:
+                if (ivUp.isSelected()) {
+                    listener.selectPrizeUp(sortType);
+                } else {
+                    listener.selectPrizeDown(sortType);
+                }
+                break;
+            case 2:
+                listener.selectSales(sortType);
+                break;
+        }
+    }
+
 }
